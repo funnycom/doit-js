404edef198a10c6e9a87463c6450c9478e664a6d
funnycom <funcom@gmail.com>
2022-04-09 00:15:44 +0900

    소스 정리

--- 13/js/solution-2.js
+++ 13/js/solution-2.js
@@ -4,7 +4,7 @@ fetch(quotesURL)
   .then(response => response.json())
   .then(data => {
     const result = document.querySelector("#result");
-    const random = Math.floor(Math.random() * 30 + 1);
+    const random = Math.floor(Math.random() * 30);  // 0 ~ 29 사이의 수
     result.querySelector(".quote").innerHTML = data.quotes[random].quote;
     result.querySelector(".author").innerHTML = ` - ${data.quotes[random].author}`;
   })
