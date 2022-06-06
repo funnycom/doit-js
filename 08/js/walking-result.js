4be680bcb11244af913e229eb72733014bd1d8c4
funnycom <funcom@gmail.com>
2022-06-06 15:38:33 +0900

    소스 정리

--- 08/js/walking-result.js
+++ 08/js/walking-result.js
@@ -3,6 +3,6 @@ const firstDay = new Date("2021-07-01");  // 시작한 날
 const today = new Date();  // 오늘
 
 let passedTime = today.getTime() - firstDay.getTime();  // 시작한 날부터 오늘까지 흐른 시간(밀리초)
-passedTime = Math.round(passedTime / (1000 * 60 * 60 * 24));  // 밀리초를 일(date) 수로 계산
+let passedDay = Math.round(passedTime / (1000 * 60 * 60 * 24));  // 밀리초를 일(date) 수로 계산
 
-result.innerText = passedTime;
+result.innerText = passedDay;
