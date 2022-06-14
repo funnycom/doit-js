6841101400db51bd8599c5cabe4a219264443775
funnycom <funcom@gmail.com>
2022-06-14 17:35:48 +0900

    소스 정리

--- 12/js/student-result.js
+++ 12/js/student-result.js
@@ -3,14 +3,14 @@ xhr.open('GET', 'student.json', true);
 xhr.send();
 
 xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        let student = JSON.parse(xhr.responseText);
-        document.getElementById("result").innerHTML = `
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    let student = JSON.parse(xhr.responseText);
+    document.getElementById('result').innerHTML = `
             <h1>${student.name}</h1>
             <ul>
               <li>전공 : ${student.major}</li>
               <li>학년 : ${student.grade}</li>
             </ul>
         `;
-    }
   }
+};
