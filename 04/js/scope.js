35904be6779977245cae3e3e843d3a4cf8205bdc
Kyunghee Ko <funcom@gmail.com>
2022-12-01 21:22:18 +0900

    Update scope.js

--- 04/js/scope.js
+++ 04/js/scope.js
@@ -1,10 +1,10 @@
 function addSum(n) {
-  let sum = 0;
-  for (let i = 1; i <= n; i++) {
+  var sum = 0;
+  for (var i = 1; i <= n; i++) {
     sum += i;
   }  
   return sum;
 }
 
-const num = 3;
+var num = 3;
 console.log(`1부터 ${num}까지 더하면 : ${addSum(num)}`);  
