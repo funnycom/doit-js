44acc0881552131c235049af49f567152e950261
Kyunghee Ko <funcom@gmail.com>
2023-04-04 12:43:50 +0900

    Update remove-3.js

--- 07/js/remove-3.js
+++ 07/js/remove-3.js
@@ -4,6 +4,6 @@ for (let button of buttons) {
   button.addEventListener("click", function () {
     // 항목 클릭했을 때 실행할 함수
     // 부모 노드에서 삭제
-    this.parentNode.remove(this);
+    this.parentNode.remove();
   });
 }
