6ad86bf72c54475542d201b4755d3accdac99ef8
Kyunghee Ko <funcom@gmail.com>
2023-05-29 15:08:16 +0900

    close버튼 소스 추가

--- 06/js/modal-result.js
+++ 06/js/modal-result.js
@@ -3,5 +3,9 @@ const modalBox = document.querySelector("#modal-box");
 const close = document.querySelector("#close");
 
 open.addEventListener("click", () => {
-  modalBox.classList.toggle("active"); // 클릭하면 클래스리스트에 .active 스타일 추가
+  modalBox.classList.add("active"); // 클릭하면 클래스리스트에 .active 스타일 추가
+});
+
+close.addEventListener("click", () => {
+  modalBox.classList.remove("active"); // 클릭하면 클래스리스트에서 .active 스타일 제거
 });
